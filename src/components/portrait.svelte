10c224d05db0c9c83560a86976c71482ed797c04
Rikki-Fontaine <146212036+Rikki-Fontaine@users.noreply.github.com>
2023-11-06 14:01:28 -0500

    adjusted header text

--- src/components/portrait.svelte
+++ src/components/portrait.svelte
@@ -11,8 +11,8 @@
 
 <!--This needs a slightly better layout-->
 
-<div class="flex justify-between md:justify-around mb-2 p-2">
-    <div class="grid grid-cols-1 gap-5 mt-2">
+<div class="flex justify-center md:justify-around mb-2 p-2 text-sm md:text-base">
+    <div class="grid grid-cols-1 gap-5 mt-2 text-left">
         <span>Name: {name}</span>
         <span>Class: {playerClass}</span>
         <span>Background: {background}</span>
@@ -20,7 +20,7 @@
 
     <Avatar src={src} width="w-32" rounded="rounded-full" />
 
-    <div class="grid grid-cols-1 mt-2 gap-5">
+    <div class="grid grid-cols-1 mt-2 gap-5 text-right">
         <span>Level: {level}</span>
         <span>Race: {race}</span>
         <span>Experience: {experience}</span>
